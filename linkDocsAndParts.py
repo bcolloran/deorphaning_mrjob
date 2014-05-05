57ba1b2181f90c0f5c10ca675401a803e65577a6
bcolloran <bcolloran@mozilla.com>
2014-05-05 10:08:46 -0700

    changed internal protocol to raw

--- linkDocsAndParts.py
+++ linkDocsAndParts.py
@@ -13,42 +13,40 @@ class linkDocsAndPartsJob(MRJob):
     # HADOOP_INPUT_FORMAT="org.apache.hadoop.mapred.KeyValueTextInputFormat"
 
     INPUT_PROTOCOL = mrjob.protocol.RawProtocol
-    INTERNAL_PROTOCOL = mrjob.protocol.JSONProtocol
+    INTERNAL_PROTOCOL = mrjob.protocol.RawProtocol
     OUTPUT_PROTOCOL = mrjob.protocol.RawProtocol
 
 
 
-    def mapper(self,docId, partId):
-        if partId[0]!="p":
-            raise
-        self.increment_counter("MAPPER", "(docId,partId) in")
-        yield docId, [partId]
+    # def mapper(self,docId, partId):
+    #     self.increment_counter("MAPPER", "(docId,partId) in")
+    #     yield docId, partId
 
 
-    def combiner(self,docId, partIdIter):
-        # partIdIter should always reach the combiner as an iter of lists like:
-        #     [["pid_1"],["pid_2"],...].
-        # need to union these tuples and emit ["pid_1","pid_2",...]
-        partSetOut = set()
-        for partIdList in partIdIter:
-            partSetOut |= set(partIdList)
-        yield(docId,list(partSetOut))
+    def combiner(self,docId, partIdStringsIter):
+        # partIdStringsIter should always reach the combiner as an iter of strings like:
+        #     ["pId1","pId2|pId3|...|pId(N)",...]
+        # need to join these with pipes.
+        # partSetOut = set()
+        # for partIdList in partIdIter:
+        #     partSetOut |= set(partIdList)
+        yield docId, "|".join(partIdStringsIter)
 
 
 
     def reducer(self,docId, partSetIter):
-        # partIdIter should always reach the reducer as an iter of lists like:
-        #     [["pid_1,1","pid_1,2",...],["pid_2"],["pid_3,1",...]...].
+        # partIdIter should always reach the reducer as an iter of strings like:
+        #     ["pId|pId|...|pId","pId","pId|pId",...]
 
         linkedParts = set()
-        for partSet in partSetIter:
-            linkedParts |= set(partSet)
+        for partSetStr in partSetIter:
+            linkedParts |= set(partSetStr.split("|"))
 
         lowPart = min(linkedParts)
 
         yield(lowPart,docId)
         self.increment_counter("REDUCER", "(lowPart,docId) out")
-        self.increment_counter("REDUCER", "OVERLAPPING_PARTS",0)
+        self.increment_counter("REDUCER", "docs in OVERLAPPING_PARTS",0)
 
         if len(linkedParts)==1:
             #if there is only one part linked to this docId, there are no overlaps here, so no part-to-part touches are emitted
@@ -56,7 +54,7 @@ class linkDocsAndPartsJob(MRJob):
         else:
             #otherwise, return all the overlaps
             yield(lowPart,"|".join(linkedParts))
-            self.increment_counter("REDUCER", "OVERLAPPING_PARTS")
+            self.increment_counter("REDUCER", "docs in OVERLAPPING_PARTS")
 
 
 
